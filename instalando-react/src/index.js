7fdcbcc331a3de7d22b670ba86706a96ab55527d
talysonxx <talysonoficial12@gmail.com>
2021-07-21 18:18:50 -0300

    configurando ambiente react

--- instalando-react/src/index.js
+++ instalando-react/src/index.js
@@ -1,7 +1,6 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import "./style.css"
-
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './style.css'
 
 const App = () => {
 
@@ -12,5 +11,5 @@ const App = () => {
     }
 }
 
-const rootElement = document.getElementById("root")
+const rootElement = document.getElementById('root')
 ReactDOM.render(<App />, rootElement)

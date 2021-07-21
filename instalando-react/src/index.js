39d4cc22bcb36ff25896c05695a9af8321c71e0d
talysonxx <talysonoficial12@gmail.com>
2021-07-21 16:51:57 -0300

    primeiro git

---
+++ instalando-react/src/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import "./style.css"
+
+
+const App = () => {
+
+    return {
+        <div className="App">
+            Hello Word
+        </div>
+    }
+}
+
+const rootElement = document.getElementById("root")
+ReactDOM.render(<App />, rootElement)

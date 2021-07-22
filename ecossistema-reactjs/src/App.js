89df7f79550f4f35a97041dbea47f61aa70ebc91
talysonxx <talysonoficial12@gmail.com>
2021-07-22 18:03:44 -0300

    estados

--- ecossistema-reactjs/src/App.js
+++ ecossistema-reactjs/src/App.js
@@ -1,9 +1,40 @@
-function App() {
-  return (
-    <div>
-      OIOIOIO
-    </div>
-  );
+import React from "react";
+import { findRenderedDOMComponentWithClass } from "react-dom/cjs/react-dom-test-utils.production.min";
+
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      clock: 1000,
+      glass: 'water'
+    }
+  }
+
+  // função nativa (executa depois que o elemento é montado no DOM)
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({
+        glass: 'juice'
+      })
+    }, 2000)
+  }
+
+  changeGlass = () => {
+    this.setState({
+      glass: 'soda pop'
+    })
+  }
+
+  render(){
+    const {clock, glass} = this.state
+
+    return(
+      <div>
+        <h1>{clock}</h1>
+        <button onClick={() => this.changeGlass()}>{glass}</button>
+      </div>
+    )
+  }
 }
 
 export default App;

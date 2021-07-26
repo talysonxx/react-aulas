56077e351ea6640351a4f0b0c840ce587b7c78ef
talysonxx <talysonoficial12@gmail.com>
2021-07-26 17:59:20 -0300

    fail: eslint bugo com printtier, noa detecta falhas e se tornou inutilizavel (dnv)

---
+++ eslint-prettier-editorconfig-2/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+    const name = "lula";
+  var bolso = "100trao";
+    return (
+       <div>
+
+       </div>
+    )
+}
+
+export default App

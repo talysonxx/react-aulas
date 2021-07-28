065b0e16d0a27988896d11ac71b83e70ef7f9a22
talysonxx <talysonoficial12@gmail.com>
2021-07-28 19:22:27 -0300

    end

--- renderizacao-condicional/src/App.js
+++ renderizacao-condicional/src/App.js
@@ -1,9 +1,41 @@
 import React from 'react'
 
+const buttonA = <button>ver histórico</button>
+const buttonB = <button>cadastrar</button>
+const temClientes = true
+
 function App() {
+  const mostrarHistorico = () => (
+        <div>
+          Clique aqui para acessar o histórico dos clientes
+          {buttonA}
+        </div>
+  )
+  const cadastrarCliente = () => (
+    <div>
+    cadastre aqui {buttonB}
+    </div> 
+  )
+
+  const nomeCliente = 'simSimSIMsiMimS'
+
+  const mostrarCliente = () => {
+    if(!temClientes) return null
+
+    return(
+      <div>
+        <h1>nome do cliente: {nomeCliente}</h1>
+      </div>
+    )
+  }
+  
   return (
     <div>
+      <p>tem cliente: {temClientes ? `sim` : `não`}</p>
       <h1 id="lula12">okOK</h1>
+      {/* se clientes  for verdade o botão aparece */}
+      {temClientes ? mostrarHistorico() : cadastrarCliente()}
+      {mostrarCliente()}
     </div>
   );
 }
--- renderizacao-condicional/src/index.js
+++ renderizacao-condicional/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import '/style.css'
+import './style.css';
 
 const divHTML = window.document.getElementById('root')
 ReactDOM.render(<App />, divHTML)

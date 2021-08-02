23fcb965b0f3870f60c861f5a29eab8d926170a4
talysonxx <talysonoficial12@gmail.com>
2021-08-02 14:31:30 -0300

    use state

--- state-react-2/src/App.js
+++ state-react-2/src/App.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function App(){
+  const [numState, alterarNum] = useState(30)
+  let numero = 20
+  let cont = 0
+
+  const numeroVariavel = () =>{
+    numero++
+    if(cont == 0) window.alert('Olhe o console (F12)')
+    window.console.log('valor da variável: ' + numero)
+    cont++
+  }
 
   return(
     <>
-    oi
+      <p>Usando variável: {numero}</p>
+      <button onClick={() => numeroVariavel()}>adicionar + 1 na Variável => valor: {numero}</button>
+      
+      <p>Usando state: {numState}</p>
+      <button onClick={() => alterarNum(numState + 1)}>adicionar + 1 no state => valor: {numState}</button>
     </>
   )
 }

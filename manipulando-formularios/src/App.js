aa1dc326c7098c8a5a5ffb3f55be787ec52c1e4a
talysonxx <talysonoficial12@gmail.com>
2021-08-02 17:50:28 -0300

    manipulando formulários com use-state

---
+++ manipulando-formularios/src/App.js
@@ -0,0 +1,39 @@
+import React, {useState} from 'react'
+
+function App() {
+  // cada input do formulário deve ter seu useState
+  const [nome, setNome] = useState('')
+  const [carro, setCarro] = useState('limosine')
+
+  const handleChangeNome = ({target: {value}}) =>{
+    setNome(value)
+    window.localStorage.setItem('valor', value)
+  }
+  const handleChangeSelect = ({target: {value}}) =>{
+    setCarro(value)
+  }
+
+  return (
+    <>
+      <form>
+        <label>Dgite algo</label>
+        <br/>
+        <input type="text" value={nome} onChange={(event) => handleChangeNome(event)}></input>
+        <p>Digitou: {window.localStorage.getItem('valor')}</p>
+
+
+        <label>Selecione um carro</label>
+        <select value={carro} onChange={(event) => handleChangeSelect(event)}>
+          <option value="prisma">Prisma</option>
+          <option value="limosine">Limosine</option>
+          <option value="audio-ultron-r8">Audio Ultron R8</option>
+          <option value="carro do most-wanted">NFSMW</option>
+        </select>
+        <br/>
+        <label>Carro selecionado: {carro}</label>
+      </form>
+    </>
+  );
+}
+
+export default App;

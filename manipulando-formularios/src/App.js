828afbb8571422fbf73bcba071be145abb6d80ef
talysonxx <talysonoficial12@gmail.com>
2021-08-03 11:36:13 -0300

    functions shortened

--- manipulando-formularios/src/App.js
+++ manipulando-formularios/src/App.js
@@ -5,12 +5,23 @@ function App() {
   const [nome, setNome] = useState('')
   const [carro, setCarro] = useState('limosine')
 
-  const handleChangeNome = ({target: {value}}) =>{
+  // const handleChangeNome = ({target: {value}}) =>{
+  //   setNome(value)
+  //   window.localStorage.setItem('valor', value)
+  // }
+  // const handleChangeSelect = ({target: {value}}) =>{
+  //   setCarro(value)
+  // }
+
+  const onChangeSelect = ({target: {value}}) => {
+    setCarro(value)
+    window.console.log({value})
+  }
+
+  const onChangeName = ({target: {value}}) => {
     setNome(value)
     window.localStorage.setItem('valor', value)
-  }
-  const handleChangeSelect = ({target: {value}}) =>{
-    setCarro(value)
+    window.console.log({value})
   }
 
   return (
@@ -18,11 +29,11 @@ function App() {
       <form>
         <label htmlFor="nome">Dgite algo</label>
         <br/>
-        <input id="nome" type="text" value={nome} onChange={(event) => handleChangeNome(event)}></input>
+        <input id="nome" type="text" value={nome} onChange={onChangeName}></input>
         <p>Digitou: {window.localStorage.getItem('valor')}</p>
 
         <label htmlFor="carros">Selecione um carro</label>
-        <select id="carros" value={carro} onChange={(event) => handleChangeSelect(event)}>
+        <select id="carros" value={carro} onChange={onChangeSelect}>
           <option value="prisma">Prisma</option>
           <option value="limosine">Limosine</option>
           <option value="audio-ultron-r8">Audio Ultron R8</option>

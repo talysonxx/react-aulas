237dba361b05675a76feefe5c947b881c68606e1
talysonxx <talysonoficial12@gmail.com>
2021-08-06 15:55:45 -0300

    dois metodos para carregar paginas em react

--- navegando-entre-telas/src/App.js
+++ navegando-entre-telas/src/App.js
@@ -5,17 +5,23 @@ import Pagina0 from './Componentes/Pagina0/Pagina0'
 
 function App() {
   const [pagina, setPagina] = useState(0)
-
+  
+  // método 1
   const retornarPagina = () => {
-    if(pagina == 1){
-      return <Pagina1 onClick0={() => irParaPagina(0)} onClick2={() => irParaPagina(2)}/>
-    }else if (pagina == 2){
-      return <Pagina2 onClick0={() => irParaPagina(0)} onClick1={() => irParaPagina(1)}/>
-    }else{
+    if (pagina == 1) {
+      return (
+        <Pagina1 onClick0={() => irParaPagina(0)} onClick2={() => irParaPagina(2)}/>
+      )
+    } else if (pagina == 2){
+      return (
+        <Pagina2 onClick0={() => irParaPagina(0)} onClick1={() => irParaPagina(1)}/>
+      )
+    } else {
       return (
         <Pagina0
-        clickButton1={() => irParaPagina(1)}
-        clickButton2={() => irParaPagina(2)}/>
+          clickButton1={() => irParaPagina(1)}
+          clickButton2={() => irParaPagina(2)}
+        />
       )
     }
   }
@@ -24,11 +30,45 @@ function App() {
     setPagina(pagina)
   }
 
+
+
+
+
+  // método 2
+  // useEffect(
+  //   () => {
+  //     let url = window.location.href.split('?')[1]
+  //     setPagina(url)
+  //   }
+  // )
+
+  // function metodo2() {
+  //   if (pagina == 1) {
+  //     return <Pagina1 onClick0={() => linksPagina(0)} onClick2={() => linksPagina(2)}/>
+  //   } else if (pagina == 2) {
+  //     return <Pagina2 onClick0={() => linksPagina(0)} onClick1={() => linksPagina(1)}/>
+  //   } else {
+  //     return <Pagina0 clickButton1={() => linksPagina(1)} clickButton2={() => linksPagina(2)}/>
+  //   }
+  // }
+
+  // function linksPagina(pagina){
+  //   if (pagina == 1) {
+  //     window.open('http://localhost:3000?1', '_self')
+  //   } else if (pagina == 2) {
+  //     window.open('http://localhost:3000?2', '_self')
+  //   } else {
+  //     window.open('http://localhost:3000?0', '_self')
+  //   }
+  // }
+
+
   return (
    <>
       {retornarPagina()}
+      {/* {metodo2()} */}
    </>
-  );
+  )
 }
 
-export default App;
+export default App
--- navegando-entre-telas/src/Componentes/Pagina0/Pagina0.js
+++ navegando-entre-telas/src/Componentes/Pagina0/Pagina0.js
@@ -5,7 +5,7 @@ export default function Pagina0({clickButton1, clickButton2}) {
         <div>
             <p>tela inicial</p>
             <button onClick={clickButton1}>Ir para a página 1</button>
-            <button onClick={clickButton2}>Ir para a página 2</button>
+            <button onClick={clickButton2}>Ir para a página 1</button>
         </div>
     )
 }

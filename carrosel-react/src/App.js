3de8532ae25010d62bb7de43b9e43909e796e704
talysonxx <talysonoficial12@gmail.com>
2021-08-19 09:36:53 -0300

    projeto criado

---
+++ carrosel-react/src/App.js
@@ -0,0 +1,14 @@
+import React, {useEffect} from 'react'
+
+function App() {
+  useEffect(() => {
+    window.document.title = 'React - aulas'
+  })
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App

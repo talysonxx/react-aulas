b5331591bdd1d6b45f61ba4a287d5d53ba85b2d6
talysonxx <talysonoficial12@gmail.com>
2021-08-20 10:58:26 -0300

    criacao do projeto

---
+++ calculadora-imc/src/App.js
@@ -0,0 +1,15 @@
+import React, {useState, useEffect} from 'react'
+
+function App(){
+  useEffect(() => {
+    document.title = 'Calcular IMC'
+  })
+
+  return (
+    <div>
+      <h1>KAKA</h1>
+    </div>
+  );
+}
+
+export default App;

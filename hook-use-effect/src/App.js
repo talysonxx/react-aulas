016a6962b2f6863e4118c0fba410d0f07adcbb29
talysonxx <talysonoficial12@gmail.com>
2021-09-29 14:59:24 -0300

    use effects equivalentes a componentDidMount, WillUnmout e DidUpdate

--- hook-use-effect/src/App.js
+++ hook-use-effect/src/App.js
@@ -3,20 +3,49 @@ import React, {useState, useEffect} from 'react'
 
 const App = () => {
   const [contagem, setContagem] = useState(0)
+  const [elemento, setElemento] = useState(true)
 
-  useEffect(
-    () => {
-      window.console.log('página carregada!')
-      window.document.title = `Contagem: ${contagem}`
+  // é executado uma vez (componentDidMount)
+  // vai executar a função de retorno somente quando o component for desmontado (componentWillUnmout)
+  useEffect(() => {
+    console.log('A página foi carregada!')
+    setContagem(parseInt(localStorage.getItem('contador')))
+
+    return () => {
+      console.log('Componente desmontado')
     }
-  )
+  }, [])
 
-  return (
-    <>
-      <p>Contagem : {contagem}</p>
-      <button onClick={() => setContagem(contagem + 1)}>Iniciar contagem</button>
-    </>
-  );
+  // executado sempre que contagem é atualizado (componentDidUpdate)
+  useEffect(() => {
+    window.document.title = `Contagem: ${contagem}`
+    localStorage.setItem('contador', contagem)
+  }, [contagem])
+  
+  let time = 0
+  useEffect(() => {
+    let timer = setInterval(() => {
+      time++
+      console.log(time)
+  
+      if (time === 10) {
+        clearInterval(timer)
+        setElemento(false)
+      }
+    }, 1000)
+  }, [])
+
+
+  if (elemento) {
+    return (
+      <>
+        <p>Contagem : {contagem}</p>
+        <button onClick={() => setContagem(contagem + 1)}>Iniciar contagem</button>
+      </>
+    )
+  } else {
+    return <>Sem contador</>
+  }
 }
 
 export default App;
